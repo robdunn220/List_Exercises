eb1d881fa8b2c2b8e9fc69d3d5cdf2d913c0e372
Robert Dunn <robertdunn220@gmail.com>
2016-09-27 09:39:48 -0400

    Completed sum_num_list.py

---
+++ largest_num.py
@@ -0,0 +1,7 @@
+numbers = [1, 4, 2, 6, 8, 5, 18. 15]
+
+placeholder = 0
+
+for x in numbers:
+
+    if
---
+++ sum_num_list.py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5, 6]
+
+num_sum = 0
+
+for x in numbers:
+    num_sum += x
+print num_sum
